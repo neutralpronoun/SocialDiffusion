1b43a5143cf227aa360d88b93e1dabb289b2d857
neutralpronoun <alexowendavies@gmail.com>
2022-11-11 12:58:57 +0000

    Updates for community generation (fb dataset musae_facebook from SNAP)

--- dgd/datasets/fb_dataset.py
+++ dgd/datasets/fb_dataset.py
@@ -134,7 +134,7 @@ class FBDataset(InMemoryDataset):
         # quit()
 
     def communities_split(self, G):
-        partition = comm.louvain_communities(G, resolution = 10)
+        partition = comm.louvain_communities(G, resolution = 15)
         # partition_dict = {i:list(partition[i]) for i in range(len(partition))}
         # self.raw_paths[0] = 'facebook_large/musae_facebook_edges.json'
 
@@ -167,9 +167,9 @@ class FBDataset(InMemoryDataset):
         
         node_type_df = pd.read_csv(self.raw_paths[2])
         unique_types = np.unique(node_type_df["page_type"])
-        print(unique_types)
+        # print(unique_types)
         types = {page_type:i for i, page_type in enumerate(unique_types.tolist())}
-        print(types)
+        # print(types)
 
         for f in open(self.raw_paths[0], "r"):
             all_edges = json.loads(f)
@@ -178,7 +178,7 @@ class FBDataset(InMemoryDataset):
 
         skip = []
         for i, G in enumerate(graphs):
-            if G.number_of_nodes() > 400:
+            if G.number_of_nodes() > 300:
                 skip.append(i)
 
         suppl = tqdm(graphs)
